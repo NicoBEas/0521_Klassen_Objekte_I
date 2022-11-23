cbdc9c7b0ebfaf8a94cffe0787fecb68b055bb57
Biagio Mamone <BMAM179@more2learn.de>
2022-11-23 08:22:58 +0100

    Probe

---
+++ com/cc/java/Cat.java
@@ -0,0 +1,12 @@
+package com.cc.java;
+
+public class Cat {
+
+public void tellYourAdress(){
+    // Aufruf der Methode output() in der Klasse App
+    // this ist die Referenz des Objektes auf sich selbst
+System.out.println("Blick von innen " + this);
+    }
+
+}
+ 
